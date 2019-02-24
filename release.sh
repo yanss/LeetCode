0c0e6ae26ee09c053b062672f2abbbacbfbebbc9
fakeYanss <yanshisangc@gmail.com>
2019-02-24 23:39:04 +0800

    add releash script

--- release.sh
+++ release.sh
@@ -2,3 +2,17 @@ java -jar LeetCodeCrawler.jar
 git add .
 git commit -m 'add algorithm solutions'
 git push origin master
+
+
+########Use Guide##########
+# first add a file named 'config.json' in the root path
+# write a json object in it, as below
+# {
+#     "username": "xxx@email.com",
+#     "password": "xxx",
+#     "language": ["cpp","java","c","csharp","javascript","python","python3","ruby","swift","golang","scala","kotlin"],
+#     "outputDir": "."
+# }
+#
+# second run the bash script
+###########################
